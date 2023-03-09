f8a505cd13a0c8184129eef7d96f00f49874f43a
Deep Raj <deeprajpandey5@gmail.com>
2023-03-09 23:49:25 +0530

    - Ui Issue fixing - Crash issue fixing

--- app/src/main/java/com/ambulance/rider/Common/Common.java
+++ app/src/main/java/com/ambulance/rider/Common/Common.java
@@ -13,7 +13,8 @@ public class Common {
 
     public static final String LOG_TAG = "RIDER";
 
-    public static final String BASE_URL = "http://hiva26.com/ambulanceapi/";
+    //public static final String BASE_URL = "http://hiva26.com/ambulanceapi/";
+    public static final String BASE_URL = "http://ec2-13-235-91-70.ap-south-1.compute.amazonaws.com/ambulanceapi/";
     public static final String RIDER_URL = "rider.php";
     public static final String TOKEN_URL = "fcm.php";
     public static final String LOCATION_URL = "driverLocation.php";
--- app/src/main/java/com/ambulance/rider/ForgotPasswordFragment.java
+++ app/src/main/java/com/ambulance/rider/ForgotPasswordFragment.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -101,7 +102,7 @@ public class ForgotPasswordFragment extends Fragment implements View.OnClickList
             oldDriver.resetPassword(new VolleyJSONResponses() {
                 @Override
                 public void onSuccess(JSONObject response) {
-
+                    Log.d("fmasfnkas", "onSuccess: "+response);
                     dialog.dismiss();
                     try {
 
